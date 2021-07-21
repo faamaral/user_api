6f7179ed3c722780748efd1b58700800bbc72e4a
faamaral <fabianoamaral445@gmail.com>
2021-07-21 20:10:34 -0300

    creating post, put and delete requests

--- user_api/views/user.py
+++ user_api/views/user.py
@@ -1,6 +1,7 @@
 import os
+import json
 
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Resource, Api
 
 from user_api.file import file_rw
@@ -15,6 +16,13 @@ print(type(users))
 class ListUser(Resource):
     def get(self):
         return users
+    def post(self):
+        data = json.loads(request.data)
+        index = len(users) + 1
+        data['id'] = index
+        users.append(data)
+        file_rw.write_json('user_api/users.json', users)
+        return users[index-1]
 
 class User(Resource):
     def get(self, id):
@@ -22,13 +30,39 @@ class User(Resource):
         for list_item in users:
             if list_item['id'] == id:
                 response = list_item
-            else:
-                response = {
-                    'status': 'falha',
-                    'mensagem': 'usuário não encontrado'
-                    }
+                
         return response
 
-api.add_resource(ListUser, '/users/')
+    def put(self, id):
+        data = json.loads(request.data)
+        response = None
+        i = 0
+        for list_item in users:
+            if list_item['id'] == id:
+                users[i] = data 
+                response = users[i]
+            i+=1 
+                           
+        file_rw.write_json('user_api/users.json', users)
+        return response
+    
+    def delete(self, id):
+        response = None
+        i = 0
+        for list_item in users:
+            if list_item['id'] == id:
+                users.pop(i) 
+                response = {
+                    'status': 200,
+                    'mensagem': 'usuario excluido com sucesso'
+                }
+            i+=1 
+                           
+        file_rw.write_json('user_api/users.json', users)
+        return response
+
+    
+
+api.add_resource(ListUser, '/','/users/')
 api.add_resource(User, '/users/<int:id>/')
 
